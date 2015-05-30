e564be9b7a3f8ee7b68e5cdf61eeb308199794e9
James Brooks <jhbrooks@uchicago.edu>
2015-05-30 14:53:00 -0500

    added while loop with basic fib code, for <100

--- euler2.js
+++ euler2.js
@@ -1,3 +1,16 @@
 /* 5/30/15 */
 /* Project Euler 2, JavaScript */
-/* Sum the even terms, with value less than or equal to 4 million, of the Fibonacci sequence starting with 1 and 2 */
+/* Sum the even terms, with value less than or equal to 4 million,
+of the Fibonacci sequence starting with 1 and 2 */
+
+var n1 = 1;
+var n2 = 2;
+var holder = 0;
+var even_sum = 0;
+
+while ((n1 < 100) && (n2 < 100)) {
+	holder = n2;
+	n2 = n1 + n2;
+	n1 = holder;
+	console.log(holder);
+}
