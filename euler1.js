6f57f2129c6064d29b01391639c41f874d2c6005
James Brooks <jhbrooks@uchicago.edu>
2015-05-30 14:37:05 -0500

    full code for euler1.js

--- euler1.js
+++ euler1.js
@@ -1,3 +1,15 @@
 /* 5/30/15 */
 /* Project Euler 1, JavaScript */
 /* Sum the multiples of 3 and 5 below 1000 */
+
+var num = 3;
+var sum = 0;
+
+while (num < 1000) {
+	if ((num % 3 === 0) || (num % 5 === 0)) {
+		sum += num;
+	};
+	num++;
+};
+
+console.log(sum);
